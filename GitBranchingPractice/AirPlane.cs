b28551232b96f30ecbebaf9331b3662d1caaaf63
MichaelDSwopeVox <digitalwhiterabbithole@gmail.com>
2022-08-08 20:58:10 -0400

    add boat and manufacturer

---
+++ GitBranchingPractice/AirPlane.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitBranchingPractice
+{
+    internal class Boat:AirPlane
+    {
+        public int NumOfWings { get; set; }
+
+        public manufacturer { get; set; }
+
+
+    }
+}
---
+++ GitBranchingPractice/Boat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitBranchingPractice
+{
+    internal class Boat : Vehicle
+    {
+        Airplane airplane = new Airplane
+         public string Captain { get; set; }
+    }
+}
